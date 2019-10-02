867087acc1d5f2fcd10bca15e5bf59905c94460e
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-02 01:19:15 -0500

    added test to see if dashboard and display render, as well as test to see the correct status of the gate when it is closed/open and locked/unlocked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,20 @@
-// Test away
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import Dashboard from './Dashboard';
+
+test('it renders', () => {
+    render(<Dashboard />);
+});
+
+// test('it has a default state where gate unlocked and open', () => {
+//     const {getByText} = render(<Dashboard />);
+//     getByText(/help/i);
+//     getByText(/open/i);
+// });
+
+// test('it cannot be opened when locked', () => {
+//     const {getByText} = render(<Dashboard />);
+//     fireEvent.click(getByText(/close gate/i));
+//     fireEvent.click(getByText(/lock gate/i));
+//     fireEvent.click(getByText(/open gate/i));
+// });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,28 @@
 // Test away!
+import React from 'react';
+import { render } from 'react-testing-library';
+import Display from './Display';
+
+test('it renders', () => {
+    render(<Display />);
+});
+
+test('it displays closed if closed prop is true', () => {
+    const {getByText} = render(<Display closed={true}/>);
+    getByText(/closed/i);
+});
+
+test('it displays open if closed prop is false', () => {
+    const {getByText} = render(<Display closed={false}/>);
+    getByText(/open/i);
+});
+
+test('it displays locked if locked prop is true', () => {
+    const {getByText} = render(<Display locked={true}/>);
+    getByText(/^locked/i);
+});
+
+test('it displays unlocked if locked prop is false', () => {
+    const {getByText} = render(<Display locked={false}/>);
+    getByText(/unlocked/i);
+});
