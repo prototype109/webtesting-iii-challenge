1d5e12133596dafaf536d1d3cab9a33efcbd72ef
Brandon Nichols <brandonaronnichols@gamil.com>
2019-10-02 22:18:50 -0500

    added test to see if gate cannot be opened or closed when locked

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent } from '@testing-library/react';
 import Dashboard from './Dashboard';
 
@@ -29,4 +30,20 @@ test('it checks the state of the door when "lock gate" button clicked', () => {
         fireEvent.click(getByText(/lock gate/i));
 
         getByText(/unlock gate/i);
+});
+
+test('it checks if gate can be cannot be opened or closed when locked', () => {
+
+    const {getByText, queryByText} = render(<Dashboard />);
+    getByText(/close gate/i);
+
+    fireEvent.click(getByText(/close gate/i));
+
+    getByText(/lock gate/i);
+
+    fireEvent.click(getByText(/lock gate/i));
+
+    expect(queryByText(/close gate/i)).toBe(null);
+
+    expect(getByText(/open gate/i)).not.toBeDisabled();
 });
